d00c5b3b9d621e2cc2b2e8ed4b4540ebdeb56326
vladthemountain <vg07102004@gmail.com>
2022-06-15 16:35:48 +0300

    Progress Backup

--- server/src/main/java/ru/vtm/lilybukkit/LilyBukkit.java
+++ server/src/main/java/ru/vtm/lilybukkit/LilyBukkit.java
@@ -2,11 +2,12 @@ package ru.vtm.lilybukkit;
 
 public class LilyBukkit {
 
-    public class ServerProperties {
+    public static class ServerProperties {
         public static final String MAX_PLAYERS = "max-players";
         public static final String SERVER_PORT = "server-port";
         public static final String VIEW_DISTANCE = "view-distance";
         public static final String SERVER_IP = "server-ip";
         public static final String WHITELIST_ENABLED = "whitelist-enable";
+        public static final String MOTD = "motd";
     }
 }
