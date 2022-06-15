5c5deabfc37e838d68a24f005ffa267071feee0c
vladthemountain <vg07102004@gmail.com>
2022-06-15 13:23:39 +0300

    Backup

--- server/src/main/java/ru/vtm/lilybukkit/command/CommandHandler.java
+++ server/src/main/java/ru/vtm/lilybukkit/command/CommandHandler.java
@@ -1,18 +1,25 @@
 package ru.vtm.lilybukkit.command;
 
+import net.minecraft.src.ServerCommand;
 import org.bukkit.command.Command;
 
 import java.util.List;
 
 public class CommandHandler {
 
-    List<Command> commandsList;
+    //TODO: Rewrite so there are no static stuff
+
+    static List<Command> commandsList;
 
     public CommandHandler() {
 
     }
 
-    public void addCommand(Command command) {
+    public void addCommand(ServerCommand command) {
         commandsList.add(command);
+    }
+
+    public static ServerCommand[] getCommandsList(){
+        return commandsList.toArray(new ServerCommand[this.commandsList.size()]);
     }
 }
