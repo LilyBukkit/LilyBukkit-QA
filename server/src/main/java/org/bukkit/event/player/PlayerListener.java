911426af8f6fe062dbf5e96cedeb7a32e57d59be
vladthemountain <vg07102004@gmail.com>
2022-07-01 18:24:37 +0300

    LilyBukkit API I rewrote and/or deleted several `org.bukkit` classes to create a Lilypad-specific Bukkit build. I'm not sure if I added everything that's needed, but that should suffice for the first build(-s). I'll start working on the docs along with the LilyBukkit implementation.

--- server/src/main/java/org/bukkit/event/player/PlayerListener.java
+++ server/src/main/java/org/bukkit/event/player/PlayerListener.java
@@ -6,35 +6,40 @@ import org.bukkit.event.Listener;
  * Handles all events thrown in relation to a Player
  */
 public class PlayerListener implements Listener {
-    public PlayerListener() {}
+    public PlayerListener() {
+    }
 
     /**
      * Called when a player joins a server
      *
      * @param event Relevant event details
      */
-    public void onPlayerJoin(PlayerJoinEvent event) {}
+    public void onPlayerJoin(PlayerJoinEvent event) {
+    }
 
     /**
      * Called when a player leaves a server
      *
      * @param event Relevant event details
      */
-    public void onPlayerQuit(PlayerQuitEvent event) {}
+    public void onPlayerQuit(PlayerQuitEvent event) {
+    }
 
     /**
      * Called when a player gets kicked from the server
      *
      * @param event Relevant event details
      */
-    public void onPlayerKick(PlayerKickEvent event) {}
+    public void onPlayerKick(PlayerKickEvent event) {
+    }
 
     /**
      * Called when a player sends a chat message
      *
      * @param event Relevant event details
      */
-    public void onPlayerChat(PlayerChatEvent event) {}
+    public void onPlayerChat(PlayerChatEvent event) {
+    }
 
     /**
      * Called early in the command handling process. This event is only
@@ -42,14 +47,16 @@ public class PlayerListener implements Listener {
      *
      * @param event Relevant event details
      */
-    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {}
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+    }
 
     /**
      * Called when a player attempts to move location in a world
      *
      * @param event Relevant event details
      */
-    public void onPlayerMove(PlayerMoveEvent event) {}
+    public void onPlayerMove(PlayerMoveEvent event) {
+    }
 
     /**
      * Called before a player gets a velocity vector sent, which will "push"
@@ -57,138 +64,158 @@ public class PlayerListener implements Listener {
      *
      * @param event Relevant event details
      */
-    public void onPlayerVelocity(PlayerVelocityEvent event) {}
+    public void onPlayerVelocity(PlayerVelocityEvent event) {
+    }
 
     /**
      * Called when a player attempts to teleport to a new location in a world
      *
      * @param event Relevant event details
      */
-    public void onPlayerTeleport(PlayerTeleportEvent event) {}
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+    }
 
     /**
      * Called when a player respawns
      *
      * @param event Relevant event details
      */
-    public void onPlayerRespawn(PlayerRespawnEvent event) {}
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+    }
 
     /**
      * Called when a player interacts with an object or air.
      *
      * @param event Relevant event details
      */
-    public void onPlayerInteract(PlayerInteractEvent event) {}
+    public void onPlayerInteract(PlayerInteractEvent event) {
+    }
 
     /**
      * Called when a player right clicks an entity.
      *
      * @param event Relevant event details
      */
-    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {}
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+    }
 
     /**
      * Called when a player attempts to log in to the server
      *
      * @param event Relevant event details
      */
-    public void onPlayerLogin(PlayerLoginEvent event) {}
+    public void onPlayerLogin(PlayerLoginEvent event) {
+    }
 
     /**
      * Called when a player has just been authenticated
      *
      * @param event Relevant event details
      */
-    public void onPlayerPreLogin(PlayerPreLoginEvent event) {}
+    public void onPlayerPreLogin(PlayerPreLoginEvent event) {
+    }
 
     /**
      * Called when a player throws an egg and it might hatch
      *
      * @param event Relevant event details
      */
-    public void onPlayerEggThrow(PlayerEggThrowEvent event) {}
+    public void onPlayerEggThrow(PlayerEggThrowEvent event) {
+    }
 
     /**
      * Called when a player plays an animation, such as an arm swing
      *
      * @param event Relevant event details
      */
-    public void onPlayerAnimation(PlayerAnimationEvent event) {}
+    public void onPlayerAnimation(PlayerAnimationEvent event) {
+    }
 
     /**
      * Called when a player opens an inventory
      *
      * @param event Relevant event details
      */
-    public void onInventoryOpen(PlayerInventoryEvent event) {}
+    public void onInventoryOpen(PlayerInventoryEvent event) {
+    }
 
     /**
      * Called when a player changes their held item
      *
      * @param event Relevant event details
      */
-    public void onItemHeldChange(PlayerItemHeldEvent event) {}
+    public void onItemHeldChange(PlayerItemHeldEvent event) {
+    }
 
     /**
      * Called when a player drops an item from their inventory
      *
      * @param event Relevant event details
      */
-    public void onPlayerDropItem(PlayerDropItemEvent event) {}
+    public void onPlayerDropItem(PlayerDropItemEvent event) {
+    }
 
     /**
      * Called when a player picks an item up off the ground
      *
      * @param event Relevant event details
      */
-    public void onPlayerPickupItem(PlayerPickupItemEvent event) {}
+    public void onPlayerPickupItem(PlayerPickupItemEvent event) {
+    }
 
     /**
      * Called when a player toggles sneak mode
      *
      * @param event Relevant event details
      */
-    public void onPlayerToggleSneak(PlayerToggleSneakEvent event) {}
+    public void onPlayerToggleSneak(PlayerToggleSneakEvent event) {
+    }
 
     /**
      * Called when a player fills a bucket
      *
      * @param event Relevant event details
      */
-    public void onPlayerBucketFill(PlayerBucketFillEvent event) {}
+    public void onPlayerBucketFill(PlayerBucketFillEvent event) {
+    }
 
     /**
      * Called when a player empties a bucket
      *
      * @param event Relevant event details
      */
-    public void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {}
+    public void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {
+    }
 
     /**
      * Called when a player enters a bed
      *
      * @param event Relevant event details
      */
-    public void onPlayerBedEnter(PlayerBedEnterEvent event) {}
+    public void onPlayerBedEnter(PlayerBedEnterEvent event) {
+    }
 
     /**
      * Called when a player leaves a bed
      *
      * @param event Relevant event details
      */
-    public void onPlayerBedLeave(PlayerBedLeaveEvent event) {}
+    public void onPlayerBedLeave(PlayerBedLeaveEvent event) {
+    }
 
     /**
-     * Called when a player is teleporting in a portal (after the animation)
+     * Called when a player dashes
      *
      * @param event Relevant event details
      */
-    public void onPlayerPortal(PlayerPortalEvent event) {}
+    public void onPlayerDash(PlayerDashEvent event) {
+    }
 
     /**
-     * Called when a player is fishing
+     * Called when a player uses cheats
      *
      * @param event Relevant event details
      */
-    public void onPlayerFish(PlayerFishEvent event) {}
+    public void onPlayerCheat(PlayerCheatEvent event) {
+    }
 }
