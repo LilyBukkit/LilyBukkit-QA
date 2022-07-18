4eb81731c0d35b9e1456e41a022679f714b78b7e
vladthemountain <vg07102004@gmail.com>
2022-07-18 18:18:49 +0300

    Changed UpdateChecker to use a HTTP GET method instead of REST API library

--- server/inject/ru/vladthemountain/lilybukkit/util/UpdateChecker.java
+++ server/inject/ru/vladthemountain/lilybukkit/util/UpdateChecker.java
@@ -1,32 +1,51 @@
 package ru.vladthemountain.lilybukkit.util;
 
-import com.jcabi.github.Coordinates.Simple;
-import com.jcabi.github.Release;
-import com.jcabi.github.RtGithub;
-import net.minecraft.src.PropertyManager;
+import org.bukkit.Bukkit;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class UpdateChecker {
     public static void checkForUpdates() {
-        RtGithub github = new RtGithub();
-        int maxNumber = 0;
 
-        for (Release release : github.repos().get(new Simple("Vladg24YT", "LilyBukkit")).releases().iterate()) {
-            maxNumber = Math.max(release.number(), maxNumber);
-        }
-        File versionFile = new File(".version");
-        if (!versionFile.exists()) {
+        String url = "https://api.github.com/repos/Vladg24YT/LilyBukkit/releases/latest";
+        String query = null /*String.format("param1=%s&param2=%s", URLEncoder.encode(""))*/;
+        String charset = StandardCharsets.UTF_8.name();
+
+        JSONObject responseBody;
+        try {
+            URLConnection urlConnection = new URL(url /*+ "?" + query*/).openConnection();
+            urlConnection.setRequestProperty("Accept-Charset", charset);
+            InputStream response;
             try {
-                versionFile.createNewFile();
+                response = urlConnection.getInputStream();
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                throw new RuntimeException("Could not initialize a response InputStream for " + url, e);
             }
+            try (Scanner scanner = new Scanner(response)) {
+                try {
+                    responseBody = (JSONObject) new JSONParser().parse(scanner.useDelimiter("\\A").next());
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Could not establish a connection with " + url, e);
         }
-        PropertyManager versionProps = new PropertyManager(versionFile);
-        if (versionProps.getIntProperty("lilybukkit-version", 3) < maxNumber) {
-            System.out.print("### An update for LilyBukkit is available! ###");
+
+        String versionName = (String) responseBody.get("name");
+
+        if (!(Bukkit.getServer().getName() + " " + Bukkit.getServer().getVersion()).equals(versionName)) {
+            System.out.println("### An update for LilyBukkit is available: " + versionName + " ###");
+        } else {
+            System.out.println("### You're using the latest LilyBukkit build ###");
         }
     }
 }
